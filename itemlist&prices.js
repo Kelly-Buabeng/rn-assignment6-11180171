9e5ce98f8ea71b523afce12829cbdf405913efb7
Kelly Buabeng <buabengkelly@gmail.com>
2024-07-03 21:44:13 +0000

    Setup

---
+++ itemlist&prices.js
@@ -0,0 +1,58 @@
+export const items = [
+  {
+      id: 1,
+      name: "Office Wear",
+      image: require("./assets/dress1.png"),
+      price: "$120",
+      description: "reversible angora cardigan"
+  },
+  {
+      id: 2,
+      name: "Black",
+      image: require(`./assets/dress2.png`),
+      price: "$120",
+      description: "reversible angora cardigan"
+  },
+  {
+      id: 3,
+      name: "Church Wear",
+      image: require("./assets/dress3.png"),
+      price: "$120",
+      description: "reversible angora cardigan"
+  },
+  {
+      id: 4,
+      name: "Lamerei",
+      image: require(`./assets/dress4.png`),
+      price: "$120",
+      description: "reversible angora cardigan"   ,
+  },
+  {
+      id: 5,
+      name: "21WN",
+      image: require(`./assets/dress5.png`),
+      price: "$120",
+      description: "reversible angora cardigan" 
+  },
+  {
+      id: 6,
+      name: "Lopo",
+      image: require(`./assets/dress6.png`),
+      price: "$120",
+      description: "reversible angora cardigan"   
+  },
+  {
+      id: 7,
+      name: "21WN",
+      image: require(`./assets/dress7.png`),
+      price: "$120",
+      description: "reversible angora cardigan"   
+  },
+  {
+      id: 8,
+      name: "lame",
+      image: require("./assets/dress3.png"),
+      price: "$120",
+      description: "reversible angora cardigan"
+  }
+]
